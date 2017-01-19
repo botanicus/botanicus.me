6157d9ea96638b0e31cb65a9919315460c4ea94d
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2017-01-19 04:48:33 -0300

    Fixed.

--- blog.botanicus.me/src/constants.js
+++ blog.botanicus.me/src/constants.js
@@ -1,11 +1,10 @@
-let API_BASE_URL;
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    API_BASE_URL = 'http://api.botanicus.dev:8000';
-    break;
-  default:
-    API_BASE_URL = 'http://api.botanicus.me';
+const getAPIBaseURL = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return 'http://api.botanicus.dev:8000';
+    default:
+      return 'http://api.botanicus.me';
+  }
 }
 
-export { API_BASE_URL };
+export const API_BASE_URL = getAPIBaseURL();
--- blog.botanicus.me/src/utils.js
+++ blog.botanicus.me/src/utils.js
@@ -1,5 +1,7 @@
-import { API_BASE_URL } from 'constants';
+import { API_BASE_URL } from './constants';
 
+
+console.log(['API_BASE_URL', API_BASE_URL]);
 export const serverURL = (path) => {
   return `${API_BASE_URL}${path}`;
 }
